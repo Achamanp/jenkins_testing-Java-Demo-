a56c0941901a7da4decb220881acb2528a50c4c4
Aachaman <144558579+Achamanp@users.noreply.github.com>
2025-11-01 14:14:23 +0530

    Updated code with new changes

--- Main.java
+++ Main.java
@@ -7,5 +7,7 @@ public class Main{
         System.out.println(new Date());
         System.out.println("Added one file...");
         System.out.println("Again added one more file...");
+        Student st = new Student();
+        st.sayHello();
     }
 }
