9d6604e9db85c66521bff6a208162b4f583478bf
Achamanp <144558579+Achamanp@users.noreply.github.com>
2025-11-04 17:41:27 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -4,7 +4,7 @@ public class Main{
     public static void main(String[] args) {
         System.out.println("This is java program for testing jenkins");
         System.out.println("Output from java program");
-        System.out.println(new Date));
+        System.out.println(new Date());
         System.out.println("Added one file...");
         System.out.println("Again added one more file...");
         Student st = new Student();
